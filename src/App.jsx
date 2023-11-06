a5e1d3d27bbfc747f7934ff1fcaee0bee3b80ef9
fakaha <lunastra34@gmail.com>
2023-11-06 20:40:52 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Deploy Pertama</h1>
+      <h1>Deploy Kedua</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
